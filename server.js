dd1739c60fbb87bbd70ddcc5fcfedfeee6e234ec
Marcell Pokorny <pokorny7477@gmail.com>
2022-09-24 23:51:14 +0200

    articles

--- server.js
+++ server.js
@@ -7,7 +7,14 @@ app.set("view engine", "ejs");
 app.use("/articles", articleRouter);
 
 app.get("/", (req, res) => {
-  res.render("index");
+  const articles = [
+    {
+      title: "Test Article",
+      date: Date.now(),
+      description: "Test Description",
+    },
+  ];
+  res.render("index", { articles: articles });
 });
 
 app.listen(5000);
