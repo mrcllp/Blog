c36c2a097604fc5623276b9506832d9dc2f20346
Marcell Pokorny <pokorny7477@gmail.com>
2022-09-25 20:58:36 +0200

    articles

--- routes/articles.js
+++ routes/articles.js
@@ -7,4 +7,8 @@ router.get("/", (req, res) => {
   res.send("In articles");
 });
 
+router.get('/new', (req, res) => {
+  res.render('articles/new')
+})
+
 module.exports = router;
--- server.js
+++ server.js
@@ -10,16 +10,16 @@ app.get("/", (req, res) => {
   const articles = [
     {
       title: "Test Article",
-      date: Date.now(),
+      date: new Date(),
       description: "Test Description",
     },
     {
       title: "Test Article 2",
-      date: Date.now(),
+      date: new Date(),
       description: "Test Description 2",
     },
   ];
-  res.render("index", { articles: articles });
+  res.render("articles/index", { articles: articles });
 });
 
 app.listen(5000);
