a28c0ceeca3c9e333abb226c318e1cec4b761ac1
Marcell Pokorny <pokorny7477@gmail.com>
2022-09-25 21:33:21 +0200

    redirect new article

--- routes/articles.js
+++ routes/articles.js
@@ -8,15 +8,17 @@ router.get("/", (req, res) => {
 });
 
 router.get('/new', (req, res) => {
-  res.render('articles/new')
+  res.render('articles/new',{article: new Article()} )
 })
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', async (req, res) => {
+  const article = await Article.findById(req.params.id)
+  if (article == null) res.redirect('/')
+  res.render('articles/show', {article: article})
 })
 
 router.post('/', async (req, res) => {
-  const article = new Article({
+  let article = new Article({
     title: req.body.title,
     description: req.body.title,
     markdown: req.body.title
