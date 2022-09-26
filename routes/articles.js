c4023b8bb9bf539feeabc2b51db019b20b5cad68
Marcell Pokorny <pokorny7477@gmail.com>
2022-09-26 13:00:13 +0200

    hmmm

--- routes/articles.js
+++ routes/articles.js
@@ -1,36 +1,51 @@
 // Roures to articles
+const express = require('express')
 const Article = require('./../models/article')
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.send("In articles");
-});
+const router = express.Router()
 
 router.get('/new', (req, res) => {
-  res.render('articles/new',{article: new Article()} )
+  res.render('articles/new', { article: new Article() })
 })
 
-router.get('/:id', async (req, res) => {
+router.get('/edit/:id', async (req, res) => {
   const article = await Article.findById(req.params.id)
-  if (article == null) res.redirect('/')
-  res.render('articles/show', {article: article})
+  res.render('articles/edit', { article: article })
 })
 
-router.post('/', async (req, res) => {
-  let article = new Article({
-    title: req.body.title,
-    description: req.body.title,
-    markdown: req.body.title
-  })
+router.get('/:slug', async (req, res) => {
+  const article = await Article.findOne({ slug: req.params.slug })
+  if (article == null) res.redirect('/')
+  res.render('articles/show', { article: article })
+})
 
-try {
-  article = await article.save()
-  res.redirect(`/articles/${article.id}`)
-} catch (e) {
-  res.render('articles/new', {article: article})
+router.post('/', async (req, res, next) => {
+  req.article = new Article()
+  next()
+}, saveArticleAndRedirect('new'))
+
+router.put('/:id', async (req, res, next) => {
+  req.article = await Article.findById(req.params.id)
+  next()
+}, saveArticleAndRedirect('edit'))
+
+router.delete('/:id', async (req, res) => {
+  await Article.findByIdAndDelete(req.params.id)
+  res.redirect('/')
+})
+
+function saveArticleAndRedirect(path) {
+  return async (req, res) => {
+    let article = req.article
+    article.title = req.body.title
+    article.description = req.body.description
+    article.markdown = req.body.markdown
+    try {
+      article = await article.save()
+      res.redirect(`/articles/${article.slug}`)
+    } catch (e) {
+      res.render(`articles/${path}`, { article: article })
+    }
+  }
 }
 
-})
-
-module.exports = router;
+module.exports = router
--- server.js
+++ server.js
@@ -1,24 +1,23 @@
-const express = require("express");
-const articleRouter = require("./routes/articles");
+const express = require('express')
 const mongoose = require('mongoose')
 const Article = require('./models/article')
-const app = express();
+const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
+const app = express()
 
-mongoose.connect('mongodb://localhost/blog', 
-{useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb://localhost/blog', {
+  useNewUrlParser: true, useUnifiedTopology: true, 
+})
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
 
-app.use(express.urlencoded({extended: false}))
-app.use("/articles", articleRouter);
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
+})
 
+app.use('/articles', articleRouter)
 
-app.get("/", async (req, res) => {
-  const articles = await Article.find()
-  res.render("articles/index", { articles: articles });
-});
-
-app.listen(5000);
-
-
-app.use("/articles", articleRouter);
+app.listen(5000)
